11cadc61fad14b710d4bc097a4998c6c5b6aa158
Jonathan Strickland <djanatyn@gmail.com>
2021-12-01 04:55:52 -0500

    day1: oops i didn't realize there was a part 2

--- haskell/src/Day1.hs
+++ haskell/src/Day1.hs
@@ -20,7 +20,18 @@ relativeDepths :: [Int] -> [RelativeDepth]
 relativeDepths depths =
   scanl Day1.compare (RelativeDepth {depth = head depths, increase = False}) (tail depths)
 
+windows :: [Int] -> [[Int]]
+windows depths = let
+  measures = take 3 depths
+  in case measures of
+       [] -> []
+       otherwise -> measures : (windows $ tail depths)
+
 main :: IO ()
 main = do
-  result <- relativeDepths <$> inputLines
-  print . length $ filter increase result
+  question1 <- relativeDepths <$> inputLines
+  -- question 1
+  print . length $ filter increase question1
+  -- question 2
+  question2 <- relativeDepths . fmap sum . windows <$> inputLines
+  print . length $ filter increase question2
