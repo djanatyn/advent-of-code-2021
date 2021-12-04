827064a083f9db1354a07239e349cfd7998bdb5e
Jonathan Strickland <djanatyn@gmail.com>
2021-12-03 22:12:58 -0500

    day3: part 2

--- haskell/src/Day03.hs
+++ haskell/src/Day03.hs
@@ -14,6 +14,11 @@ type Record = [Char]
 update :: [Record] -> Record -> [Record]
 update accum new = zipWith (:) new accum
 
+opposite :: Char -> Char
+opposite '0' = '1'
+opposite '1' = '0'
+opposite _ = error "bad input"
+
 mostCommon :: String -> Char
 mostCommon string
   | zeroes > ones = '0'
@@ -23,10 +28,8 @@ mostCommon string
     zeroes = length $ intersect string "0"
     ones = length $ intersect string "1"
 
-opposite :: Char -> Char
-opposite '0' = '1'
-opposite '1' = '0'
-opposite _ = error "bad input"
+readBinary :: Record -> Int
+readBinary = fst . head . readBin
 
 solve :: [Record] -> Int
 solve input =
@@ -35,13 +38,59 @@ solve input =
       epsilon = fst . head . readBin $ map (opposite . mostCommon) cols
    in gamma * epsilon
 
+data BitCriteria where
+  OxygenGenerator :: BitCriteria
+  CO2Scrubber :: BitCriteria
+  deriving (Show)
+
+rank :: BitCriteria -> String -> Char
+rank criteria string
+  | zeroes > ones = case criteria of
+    OxygenGenerator -> '0'
+    CO2Scrubber -> '1'
+  | ones > zeroes = case criteria of
+    OxygenGenerator -> '1'
+    CO2Scrubber -> '0'
+  | zeroes == ones = case criteria of
+    OxygenGenerator -> '1'
+    CO2Scrubber -> '0'
+  | otherwise = error "failed to find rank"
+  where
+    zeroes = length $ intersect string "0"
+    ones = length $ intersect string "1"
+
+solve2 :: BitCriteria -> [Record] -> Record
+solve2 criteria input =
+  let step :: Int -> [Record] -> Record
+      step index rest =
+        let bits = map (!! index) rest
+            next = filter ((== rank criteria bits) . (!! index)) rest
+         in case next of
+              [last] -> last
+              [] -> error "failed"
+              _ -> step (index + 1) next
+   in step 0 input
+
 testExample :: TestTree
 testExample = testCase "example" $ do
   example <- lines <$> readFile "../input/day3-example.txt"
   solve example @?= 198
 
+testExample2 :: TestTree
+testExample2 = testCase "example" $ do
+  example <- lines <$> readFile "../input/day3-example.txt"
+  solve2 OxygenGenerator example @?= "10111"
+  solve2 CO2Scrubber example @?= "01010"
+
 main :: IO ()
-main = catch (defaultMain testExample) $
+main = catch (defaultMain $ testGroup "examples" [testExample, testExample2]) $
   \case
-    ExitSuccess -> lines <$> getContents >>= print . solve
+    ExitSuccess -> do
+      input <- lines <$> getContents
+      -- question 1
+      print $ solve input
+      -- question 2
+      print $
+        (readBinary $ solve2 OxygenGenerator input)
+          * (readBinary $ solve2 CO2Scrubber input)
     _ -> return ()
